8bf0355c56c96c183332b8ebd2fce9f8b7236289
Frederic Rouge <frederic.rouge@ikmail.com>
2026-09-24 13:07:44 +0200

    refactor: fix EntityCategory import

--- custom_components/hacs_refresh/sensor.py
+++ custom_components/hacs_refresh/sensor.py
@@ -7,8 +7,8 @@ from typing import Any
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.util import dt as dt_util
 
